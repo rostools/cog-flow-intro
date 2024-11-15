51d94436361ebe32126e20c8c3f416c787647864
Luke W. Johnston <lwjohnst@gmail.com>
2024-11-15 11:25:07 +0100

    chore: :fire: can build slides by using a symlink, so removing these first

--- _build-slides.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script is to fix an issue with using extensions when trying
-# to render a qmd outside of the project specs. Specifically, for
-# creating slides.
-cp slides/introduction.qmd _introduction.qmd
-
-quarto render _introduction.qmd --to r3-theme-revealjs
-
-mv _introduction.html slides/introduction.html
